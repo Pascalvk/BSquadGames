f9155e3f45bbf687f8eefa2ba9285558b55dba55
Pascalvk <40421967+Pascalvk@users.noreply.github.com>
2025-05-16 22:54:43 +0200

    changed names + added diagonal

--- BSquadTesting/ConnectFour/BoardTests.cs
+++ BSquadTesting/ConnectFour/BoardTests.cs
@@ -63,7 +63,7 @@ namespace BSquadTesting.ConnectFour
         }
 
         [TestMethod]
-        public void CheckWin_DiagonalWin_ReturnsTrue()
+        public void CheckWin_DiagonalUpLeftWin_ReturnsTrue()
         {
             ConnectFourBoard board = new();
 
@@ -71,6 +71,19 @@ namespace BSquadTesting.ConnectFour
             board.SetDiscAt(1, 1, 1);
             board.SetDiscAt(2, 2, 1);
             board.SetDiscAt(3, 3, 1);
+
+            Assert.IsTrue(board.CheckWin(1));
+        }
+
+        [TestMethod]
+        public void CheckWin_DiagonalUpRightWin_ReturnsTrue()
+        {
+            ConnectFourBoard board = new();
+
+            board.SetDiscAt(3, 0, 1);
+            board.SetDiscAt(2, 1, 1);
+            board.SetDiscAt(1, 2, 1);
+            board.SetDiscAt(0, 3, 1);
 
             Assert.IsTrue(board.CheckWin(1));
         }
