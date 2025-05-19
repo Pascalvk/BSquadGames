8a8b8c1375c10cadffc68aaf9fa310e42007aa22
Pascalvk <40421967+Pascalvk@users.noreply.github.com>
2025-05-19 23:18:40 +0200

    Added some visuals and updated the vertical check to make sure it doesnt go out of bounds.

--- BSquadGames/Classes/ConnectFour/ConnectFourBoard.cs
+++ BSquadGames/Classes/ConnectFour/ConnectFourBoard.cs
@@ -5,19 +5,19 @@
         public int Rows = 6;
         public int Columns = 7;
         const int WinLenght = 4;
+        public bool GameWon = false;
+        public int GameWinner;
 
-        private int[,] grid;
+        public int[,] grid;
 
         public ConnectFourBoard()
         {
-
-
             // Create 6x7 grid
             grid = new int[Rows, Columns];
         }
 
         // Check for 4 in a row, based on player number. empty = 0; player 1 = 1; player 2 = 2
-        public bool CheckWin(int player)
+        private bool CheckWin(int player)
         {
             // Check horizontal
             for (int i = 0; i < Rows; i++)
@@ -32,9 +32,9 @@
             }
 
             // Check vertical
-            for (int i = 0; i < Columns; i++)
+            for (int i = 0; i <= Rows - WinLenght; i++)
             {
-                for (int j = 0; j <= Rows - WinLenght; j++)
+                for (int j = 0; j < Columns; j++)
                 {
                     if (grid[i, j] == player && grid[i + 1, j] == player && grid[i + 2, j] == player && grid[i + 3, j] == player)
                     {
@@ -53,5 +53,30 @@
         {
             grid[row, col] = player;
         }
+
+        public void StartNewGame()
+        {
+            grid = new int[Rows, Columns];
+        }
+
+        public void CheckWinner()
+        {
+            if (CheckWin(1) == true)
+            {
+                GameWon = true;
+                GameWinner = 1;
+            }
+            else if (CheckWin(2) == true)
+            {
+                GameWon = true;
+                GameWinner = 2;
+            }
+            else
+            {
+                GameWon = false;
+                GameWinner = 0;
+            }
+        }
+
     }
 }
