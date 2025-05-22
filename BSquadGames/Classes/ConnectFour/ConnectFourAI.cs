d2e7f059981444c7d50a801f63e0b4583631b75d
Pascalvk <40421967+Pascalvk@users.noreply.github.com>
2025-05-22 21:41:33 +0200

    Added a very simple ai

---
+++ BSquadGames/Classes/ConnectFour/ConnectFourAI.cs
@@ -0,0 +1,21 @@
+﻿namespace BSquadGames.Classes.ConnectFour
+{
+    public class ConnectFourAI
+    {
+        private ConnectFourGameManager GameManager;
+
+        public ConnectFourAI(ConnectFourGameManager gameManager)
+        {
+            GameManager = gameManager;
+        }
+
+        public void MakeAnAIMove()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 6);
+
+            (int, int) moveToMake = GameManager.ValidMoves[randomNumber];
+            GameManager.MakeMove(moveToMake);
+        }
+    }
+}
